a17f9fd8254fb04ac75254a96f43470b17657697
Narcodz <naziknassar@gmail.com>
2023-06-22 00:32:42 +0530

    first production commit

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { isAdmin, requireSignIn } from './../middlewares/authMiddleware.js';
+import { categoryController, createCategoryController, deleteCategoryController, singleCategoryController, updateCategoryController } from "../controllers/categoryController.js";
+
+const router = express.Router()
+
+//routes
+//create category
+router.post('/create-category', requireSignIn, isAdmin, createCategoryController)
+
+//update category
+router.put('/update-category/:id', requireSignIn, isAdmin, updateCategoryController)
+
+//Get All category
+router.get('/get-category', categoryController)
+
+//Single category
+router.get('/single-category/:slug', singleCategoryController)
+
+//Delete category
+router.delete('/delete-category/:id', requireSignIn, isAdmin, deleteCategoryController)
+export default router
